dfa7c72ba93da90efdb21233e802c8a494d805ae
xcrystal <xcrystal627@gmail.com>
2024-03-27 20:05:25 -0400

    add: snp-oracle

---
+++ crossvals/snp-oracle/snp.py
@@ -0,0 +1,47 @@
+import bittensor as bt
+from base.synapse_based_crossval import SynapseBasedCrossval
+from protocol import Challenge
+from datetime import datetime, timedelta
+import time
+from pytz import timezone
+
+
+class SnpOracleCrossval(SynapseBasedCrossval):
+
+    def __init__(self, netuid = 28, wallet_name = 'my_wallet', wallet_hotkey = 'my_first_hotkey', network = "finney", topk = 1):
+
+        super().__init__(netuid, wallet_name, wallet_hotkey, network, topk)
+
+        self.dendrite = bt.dendrite( wallet = self.wallet )
+
+    def forward(self, timeout: float,):
+
+        ny_timezone = timezone('America/New_York')
+        current_time_ny = datetime.now(ny_timezone)     
+        current_time_ny = datetime.now(ny_timezone)
+        timestamp = current_time_ny.isoformat()
+
+        synapse = Challenge(
+            timestamp=timestamp,
+        )
+
+
+        axons = [self.metagraph.axons[i['uid']] for i in self.top_miners]
+
+        responses = self.dendrite.query(
+            axons=axons,
+            synapse=synapse,
+            timeout=timeout,
+        )
+    
+        return responses
+    
+
+    def run(self):
+        response = self.forward(60)
+        return response
+
+
+snp_crossval = SnpOracleCrossval()
+
+print(snp_crossval.run())
